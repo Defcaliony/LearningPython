bcd0848f5dc71e83329896d0fc30d767086cf8ba
Defcaliony <defcalion@yahoo.com>
2026-07-20 21:59:54 +0300

    Initial commit

---
+++ N_prc_variables_gemini_200726.py
@@ -0,0 +1,21 @@
+import random as rnd #used the module
+
+secret_number = rnd.randint(1,100)  #add random number
+#user_number = int(input("Which number do you want? "))
+user_number = 0  #create the var for cycle
+attempts = 0  #initial number of attempts
+
+print("I'm thinking of a number betwen 1 and 100")   #last com
+
+while user_number != secret_number:
+    user_number = int(input("Enter your number: "))
+    attempts += 1  #add 1 to attempts
+
+    if user_number == secret_number:
+        print("Correct")
+    elif user_number > secret_number:
+        print("Too high")
+    else:
+        print("Not enough!")
+
+print("You have used", attempts, "attempts")
