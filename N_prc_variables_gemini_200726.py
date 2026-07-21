f2820323a27eca101b046f4555ee616c316955b6
Defcaliony <defcalion@yahoo.com>
2026-07-21 14:46:15 +0300

    Initial commit

--- N_prc_variables_gemini_200726.py
+++ N_prc_variables_gemini_200726.py
@@ -7,8 +7,14 @@ attempts = 0  #initial number of attempts
 
 print("I'm thinking of a number betwen 1 and 100")   #last com
 
-while user_number != secret_number:                    #cucle<
-    user_number = int(input("Enter your number: "))
+
+while user_number != secret_number:                    #cycle<
+    try:                                                #cycle for error<
+        user_number = int(input("Enter your number: "))
+    except ValueError:
+        print("You Wrong, try again for number:")
+        continue  # go to start cycle before error      #>cycle for error
+
     attempts += 1  #add 1 to attempts
 
     if user_number == secret_number:
@@ -16,6 +22,13 @@ while user_number != secret_number:                    #cucle<
     elif user_number > secret_number:
         print("Too high")
     else:
-        print("Not enough!")                           #>cucle
+        print("Not enough!")                           #>cycle
 
 print("You have used", attempts, "attempts")
+
+
+#while user_number is None:
+#    try:
+#        user_number = int(input("Enter a number: "))
+#    except ValueError:
+#        print("You Wrong, try again to number")
