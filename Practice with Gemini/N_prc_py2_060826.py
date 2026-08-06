75b74ce2f26b55591e2e5ae68e3a27a409dd9050
Defcaliony <defcalion@yahoo.com>
2026-08-06 16:53:05 +0300

    Initial commit

---
+++ Practice with Gemini/N_prc_py2_060826.py
@@ -0,0 +1,6 @@
+items = ["Oil", "Philter", "Candle"]
+prices = ["1200", "350", "500"]
+
+for i, q in enumerate(items):   # i == index; q == item(s)
+    print(f"{i+1}. Item {q}:  | Price: {prices[i]}")
+
