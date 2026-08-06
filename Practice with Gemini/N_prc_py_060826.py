a4d58be4c182f7ec9e95d286cb7ece4c881bd4df
Defcaliony <defcalion@yahoo.com>
2026-08-06 16:34:52 +0300

    Initial commit

---
+++ Practice with Gemini/N_prc_py_060826.py
@@ -0,0 +1,9 @@
+#name = "Alex"
+#apples = 5
+fruits = ["apple", "orange", "banana"]
+answers = ["4", "2","3"]
+
+for i, q in enumerate(fruits):
+    print(f"Fruits: {q} | Amount: {answers[i]}")
+
+#print(f"Hello {name}, you have {len(fruits)} fruits")
