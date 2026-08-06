70ba4de7be605c5f18cb9b2843f16d942a2ed8d7
Defcaliony <defcalion@yahoo.com>
2026-08-06 17:04:24 +0300

    Initial commit

--- Practice with Gemini/N_prc_py2_060826.py
+++ Practice with Gemini/N_prc_py2_060826.py
@@ -2,6 +2,6 @@ items = ["Oil", "Philter", "Candle"]
 prices = [1200, 350, 500]
 
 for i, q in enumerate(items):   # i == index; q == item(s)
-    print(f"{i+1}. Item {q}:  | Price: {prices[i]} hrn")
+    print(f"{i+1}. Item {q}:  | Price: {prices[i]} UAH")
 
-print("Total: ", sum(prices), "HRN")
+print("Total: ", sum(prices), "UAH")
