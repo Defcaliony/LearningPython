45e74f1e406b23a944a0a59575c04edf0b8fbf93
Defcaliony <defcalion@yahoo.com>
2026-08-10 17:03:55 +0300

    Initial commit

--- Practice with Gemini/a1_prc_100826.py
+++ Practice with Gemini/a1_prc_100826.py
@@ -7,6 +7,14 @@ for i,q in enumerate(items):
     print(f"{i+1}. Item {q} x{quantities[i]}:  | Price: {prices[i] * quantities[i]} UAH")
     total_sum += prices[i] * quantities[i]
 
+if total_sum > 3000:
+    discount = total_sum * 0.10
+    final_total = total_sum - discount # some difficult
 
-
-print("Total: ",total_sum) #sum(prices)
+    print(f"Total: {total_sum} UAH" )
+    print(f"Discount(10%): -{discount} UAH")
+    print(f"Final total: {final_total} UAH")
+else:
+    print(f"Total: {total_sum} UAH")
+ #sum(prices)
+print("Total items:", sum(quantities), "pcs" )
