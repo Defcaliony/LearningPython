848a74813d6a072c8f8c249904f142fed984c4be
Defcaliony <defcalion@yahoo.com>
2026-08-23 15:24:07 +0300

    Initial commit

--- Practice with Gemini/a4_prc_G_pw_170826.py
+++ Practice with Gemini/a4_prc_G_pw_170826.py
@@ -4,13 +4,18 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=False, slow_mo=2000)
     page = browser.new_page()
 
-    print("Open website")
-    page.goto("https://example.com")
+    #print("Open website")
+    page.goto("https://uk.wikipedia.org")
 
-    title = page.title()
-    print(f"Title website: {title} ")
+    #title = page.title()
+    #print(f"Title website: {title} ")
+    #search_input = page.locator("input[type='search']")
+    search_input = page.get_by_placeholder("Пошук у Вікіпедії").first  #search
 
-    page.screenshot(path="example.png")
-    print("Screenshot 'Example.png' saved")
+    search_input.fill("Python")
+    search_input.press("Enter")
+
+    page.screenshot(path="Python test.png")
+    print(f"New title: {page.title()}")
 
     browser.close()
