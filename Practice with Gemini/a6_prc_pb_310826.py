7238fbc81eef7ce3a59877ade3b916cb8a795f0a
Defcaliony <defcalion@yahoo.com>
2026-08-31 15:32:17 +0300

    Initial commit

--- Practice with Gemini/a6_prc_pb_310826.py
+++ Practice with Gemini/a6_prc_pb_310826.py
@@ -4,12 +4,19 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=False, slow_mo=2000)
     page = browser.new_page()
 
-    print("Open Wiki...")
+    #print("Open Wiki...")
     page.goto("https://uk.wikipedia.org")
 
-    print("Click button 'Вікіпедія:Проєкти'...")
-    page.get_by_text("Проєкти").first.click()
+    #print("Click button 'Вікіпедія:Проєкти'...")
+    #page.get_by_text("Проєкти").first.click()
+    search_input = page.locator("#searchInput")
+    search_input.fill("Python")
+    search_input.press("Enter")
 
-    print(f"New Title: {page.title()}")
+    current_title = page.title()
+    print(f"This Title: {current_title}")
+
+    assert "Python" in current_title, "Error: Title not found!"
+    print("Test success: Title correct")
 
     browser.close()
